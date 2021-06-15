0ecf2c7ff8e229b68863bb49f4ed0f68e9789d15
Kelly Selden <kellyselden@gmail.com>
2021-06-15 14:09:37 -0700

    fix: ignore `dynamicImport` false errors

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ module.exports = {
   rules: {
     'strict': 'error',
 
+    'node/no-unsupported-features/es-syntax': ['error', {
+      'ignores': [
+        // remove once https://github.com/mysticatea/eslint-plugin-node/issues/250 is fixed
+        'dynamicImport',
+      ],
+    }],
+
     'json-files/require-engines': 'error',
     'json-files/require-license': 'error',
     'json-files/restrict-ranges': ['error', { pinUnstable: true }],
