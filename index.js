24bab6ae3cb5e7d67417fc323de35e2d3fdedf31
Charlie-Crowdstrike <126509736+Charlie-Crowdstrike@users.noreply.github.com>
2024-07-25 15:09:13 -0700

    chore: upgrade eslint-config-n to 17.9.0, along with setting peerDeps… (#92)
    feat!: upgrade eslint-config-n to 17.x.x, set relevant peerDep, and remove unneeded legacy rule

--- index.js
+++ index.js
@@ -14,14 +14,6 @@ module.exports = {
   rules: {
     'strict': 'error',
 
-    'n/no-unsupported-features/es-syntax': ['error', {
-      'ignores': [
-        // remove once we upgrade to v17
-        // https://github.com/eslint-community/eslint-plugin-n/issues/80
-        'dynamicImport',
-      ],
-    }],
-
     'json-files/require-engines': 'error',
     'json-files/require-license': 'error',
     'json-files/restrict-ranges': ['error', { pinUnstable: true }],
