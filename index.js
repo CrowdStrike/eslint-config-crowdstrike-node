3f6d92d4d9fffa541b889ebe5ed41a9cbdb17122
Charles Lim <charles.lim@crowdstrike.com>
2024-07-25 14:33:14 -0700

    fix: re-add in whitespace

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ module.exports = {
   ],
   rules: {
     'strict': 'error',
+
     'json-files/require-engines': 'error',
     'json-files/require-license': 'error',
     'json-files/restrict-ranges': ['error', { pinUnstable: true }],
